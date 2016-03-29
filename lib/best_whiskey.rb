8b6140486834da0f95cbf2115acad707a49951a1
Brett <brettrheenan@gmail.com>
2016-03-29 22:31:49 +0200

    gem bump -v 0.1.1

--- lib/best_whiskey.rb
+++ lib/best_whiskey.rb
@@ -1,4 +1,5 @@
-
+require 'nokogiri'
+require 'open-uri'
 require 'best_whiskey/version'
 require_relative 'best_whiskey/cli'
 require_relative 'best_whiskey/contest_year'
--- lib/best_whiskey/version.rb
+++ lib/best_whiskey/version.rb
@@ -1,3 +1,3 @@
 module BestWhiskey
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
